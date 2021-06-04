961c9ebcd89bfad2dfb4a71c2c1abde78a362c95
ShakibAhmed007 <shakibahmed09112017@gmail.com>
2021-06-04 18:08:24 +0600

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { PostsComponent } from './posts/posts.component';
+import { AuthInceptorService } from './auth-interceptor.service';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, HttpClientModule],
   declarations: [AppComponent, HelloComponent, PostsComponent],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInceptorService,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
